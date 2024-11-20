235943f3dfb6cefa21dea7011df8f078f51e25cc
Austin Saylor <austin@saylors.com>
2024-11-20 16:55:05 -0700

    Final Project - Cross-subnet script

--- Final-Project/Device-Detector/Ver1/main.py
+++ Final-Project/Device-Detector/Ver1/main.py
@@ -1,28 +1,25 @@
-from scapy.all import ARP, Ether, srp
+from scapy.all import ICMP, IP, sr1, sr
 
-
-target_ip = "192.168.1.1/24"
-arp = ARP(pdst = target_ip)
-ether = Ether(dst="ff:ff:ff:ff:ff:ff")
-packet = ether/arp
+target_ip = "192.168.2.0/24"  # Change to the subnet of the Ubuntu VPC
+packet = IP(dst=target_ip)/ICMP()
 
 def print_clients(clients: list) -> None:
     print("Available devices:")
-    print("IP" + " "*18+"MAC")
+    print("IP" + " "*18+"MAC (if available)")
 
     for client in clients:
-        print("{:16}    {}".format(client['IP'], client['MAC']))
+        print("{:16}    {}".format(client['IP'], client.get('MAC', 'N/A')))
 
 def main() -> None:
-    result = srp(packet, timeout = 2)[0]
+    print("Sending ICMP Echo Requests...")
+    result = sr(IP(dst=target_ip)/ICMP(), timeout=2, verbose=0)[0]
 
     clients = []
 
     for sent, received in result:
-        clients.append({'IP': received.psrc, 'MAC': received.hwsrc})
-    
-    print_clients(clients)
+        clients.append({'IP': received.src})
 
+    print_clients(clients)
 
 if __name__ == "__main__":
     main()
