b31b27b2e5ca1729f1fe8926b4aea48c15e6a0df
Austin Saylor <austin@saylors.com>
2024-11-26 16:00:46 -0700

    Removed OS detection

--- Final-Project/Device-Detector/Ver1/main.py
+++ Final-Project/Device-Detector/Ver1/main.py
@@ -16,7 +16,7 @@ def get_ip(interface):
     try:
         ip_address = fcntl.ioctl(
             s.fileno(),
-            0x8915,  # SIOCGIFADDR
+            0x8915,
             struct.pack('256s', interface[:15].encode('utf-8'))
         )[20:24]
         return socket.inet_ntoa(ip_address)
@@ -44,17 +44,14 @@ def get_icmp_subnets(machine_ip, target_subnets):
     icmp_subnets = []
     for subnet in target_subnets:
         match = False
-        print("Scanning subnet {}...".format(subnet))
         target_ips = [str(ip) for ip in ipaddress.IPv4Network(subnet, strict=False)]
 
         for target_ip in target_ips:
-            print("Comparing IPs...")
             if (target_ip == machine_ip):
-                print("**********{} AND {} ARE EQUAL**********".format(target_ip, machine_ip))
                 match = True
                 break
             else:
-                print("{} and {} are NOT equal.".format(target_ip, machine_ip))
+                continue
         
         if match == False:
             icmp_subnets.append(target_subnets[index])
@@ -70,19 +67,19 @@ def arp_scan(subnet):
     print("Running ARP Scan...")
     target_ips = [str(ip) for ip in ipaddress.IPv4Network(subnet, strict=False)]
     try:
-        print("Sending ARP requests...")
+        print("ARP: Sending ARP requests...")
         ips = []
         macs = []
         for ip in target_ips:
-            print("Scanning {}...".format(ip))
+            print("ARP: Scanning {}...".format(ip))
             request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip)
             ans, unans = srp(request, timeout=1, retry=0, iface="eth0", verbose=0)
             for sent, received in ans:
-                print("ARP Response from IP: {}, MAC: {}".format(received.psrc, received.hwsrc))
                 #result.append({'IP': received.psrc, 'MAC': received.hwsrc})
                 ips.append(received.psrc)
                 macs.append(received.hwsrc)
         result = [ips, macs]
+        print("ARP Scan Completed!\n\n")
         return result
     except Exception as e:
         print("Error during ARP scan: {}".format(e))
@@ -95,35 +92,15 @@ def icmp_scan(target_subnets, max_hosts=10):
         for subnet in target_subnets:
             target_ips = [str(ip) for ip in ipaddress.IPv4Network(subnet, strict=False)]
             for ip in target_ips:
+                print("ICMP: Scanning {}...".format(ip))
                 response = sr1(IP(dst=ip) / ICMP(), timeout=1, verbose=0)
                 if response:
-                    print("ICMP Response from IP: {}".format(response.src))
-                    #result.append({'IP': response.src, 'MAC': '***'})
                     result.append(response.src)
+        print("ICMP Scan Completed!\n\n")
         return result
     except Exception as e:
         print("Error during ICMP scan: {}".format(e))
         return []
-
-def os_scan(target_subnets):
-    print("Running OS Scan...")
-    ## Create custom TCP SYN packet
-    result = []
-    for subnet in target_subnets:
-        target_ips = [str(ip) for ip in ipaddress.IPv4Network(subnet, strict=False)]
-        for ip in target_ips:
-            packet = IP(dst=ip)/TCP(dport=80, flags="S")
-            response = sr1(packet, timeout=2, verbose=0)
-            
-            if response:
-                ## Analyze TCP window size and TTL
-                if response.ttl <= 32:
-                    result.append("Linux/Unix")
-                elif (response.ttl > 32) and (response.ttl <= 64):
-                    result.append("Windows")
-                else:
-                    result.append("Unknown OS")
-    return result
 
 def port_scan(target_ips, p):
     print("Running port Scan...")
@@ -135,23 +112,25 @@ def port_scan(target_ips, p):
         for i in range(p):
             try:
                 connection = s.connect((target_ip, i))
-                print("Port {} is open on {}!".format(i, target_ip))
                 ports.append(i)
             except:
                 continue
         result.append(ports)
+    print("Port Scan Completed!\n\n")
     return result
 
 
 def print_results(results):
-    arp_results = results[0]
-    icmp_results = results[1]
-    os_results = results[2]
+    arp_results, icmp_results, port_results = results
 
     ips = arp_results[0] + icmp_results
     macs = arp_results[1]
 
-    print("IP" + " "*14 + "MAC" + " "*19 + "OS")
+    while len(port_results) < len(ips):
+        port_results.append([])  # Placeholder for no port results
+
+    print("\n\n" + " "*23 + "RESULTS:")
+    print("IP" + " "*14 + "MAC" + " "*19 + "Open Ports")
     print("-"*55)
 
     i = 0
@@ -161,7 +140,10 @@ def print_results(results):
             mac = macs[i]
         else:
             mac = "*"*17
-        print('{}     {}     {}'.format(ips[i], mac, os_results[i]))
+        
+        ports = ", ".join(map(str, port_results[i])) if port_results[i] else "None"
+
+        print('{}     {}     {}'.format(ips[i], mac, ports))
         i += 1
 
 
@@ -174,21 +156,15 @@ def main():
     icmp_subnets = get_icmp_subnets(machine_ip, target_subnets)
 
     arp_results = arp_scan(arp_subnet)
-    target_ips = arp_results[0]
-    print("ARP Scan Completed!\n\n")
 
     icmp_results = icmp_scan(icmp_subnets)
-    print("ICMP Scan Completed!\n\n")
 
-    os_results = os_scan(target_subnets)
+    target_ips = arp_results[0] + icmp_results
 
     port_results = port_scan(target_ips, 100)
 
-    results = [arp_results, icmp_results, os_results]
-
-    print("\n\nRESULTS:")
+    results = [arp_results, icmp_results, port_results]
     print_results(results)
-    print(port_results)
 
 
 if __name__ == "__main__":
