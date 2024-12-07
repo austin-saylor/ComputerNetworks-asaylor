e34bdec05dc9f20f4d333d0651c65b1da73a57e0
Austin Saylor <austin@saylors.com>
2024-12-06 17:20:24 -0700

    cleaned up code, added comments

--- Assignments/Lab6-ClientAndServer/server.c
+++ Assignments/Lab6-ClientAndServer/server.c
@@ -1,7 +1,3 @@
-/*
-** server.c -- a stream socket server demo
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -15,214 +11,228 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-#define PORT "8000"  // the port users will be connecting to
+#define PORT "8000" // the port to be used
+#define MAXDATASIZE 100 // max size for file transfers
+#define BACKLOG 10
 
-#define MAXDATASIZE 100 // max number of bytes we can get at once 
-#define BACKLOG 10   // how many pending connections queue will hold
-
-void sigchld_handler(int s)
+void sigchld_handler(int s) 
 {
     // waitpid() might overwrite errno, so we save and restore it:
     int saved_errno = errno;
-
-    while(waitpid(-1, NULL, WNOHANG) > 0);
-
+    while (waitpid(-1, NULL, WNOHANG) > 0);
     errno = saved_errno;
 }
 
-
 // get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
+void *get_in_addr(struct sockaddr *sa) 
 {
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
+    if (sa->sa_family == AF_INET) 
+    {
+        return &(((struct sockaddr_in *)sa)->sin_addr);
     }
-
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+    return &(((struct sockaddr_in6 *)sa)->sin6_addr);
 }
 
-int main(void)
+int setup_server() 
 {
-    int sockfd, new_fd, numbytes;  // listen on sock_fd, new connection on new_fd
-    char buf[MAXDATASIZE];
-    char msg[MAXDATASIZE];
     struct addrinfo hints, *servinfo, *p;
-    struct sockaddr_storage their_addr; // connector's address information
-    socklen_t sin_size;
-    struct sigaction sa;
-    int yes=1;
-    char s[INET6_ADDRSTRLEN];
-    int rv;
+    int rv, sockfd, yes = 1;
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
+    hints.ai_flags = AI_PASSIVE;
 
-    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) 
+    {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
+        return -1;
     }
 
-    // loop through all the results and bind to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
-            perror("[SERVER]: socket");
+    for (p = servinfo; p != NULL; p = p->ai_next) 
+    {
+        // Create a socket
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) 
+        {
+            // If socket creation fails, throw an error
+            perror("[SERVER]: socket\n");
             continue;
         }
 
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-                sizeof(int)) == -1) {
-            perror("setsockopt");
+        // Allow the socket to reuse addresses
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) 
+        {
+            perror("setsockopt\n");
             exit(1);
         }
 
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+        // Bind the socket to an address
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) 
+        {
+            // If the socket fails to bind, throw an error
             close(sockfd);
-            perror("[SERVER]: bind");
+            perror("[SERVER]: bind\n");
             continue;
         }
 
         break;
     }
 
-    freeaddrinfo(servinfo); // all done with this structure
+    freeaddrinfo(servinfo);
 
-    if (p == NULL)  {
+    if (p == NULL) 
+    {
+        // If the server fails to bind to the socket, throw an error
         fprintf(stderr, "[SERVER]: failed to bind\n");
+        return -1;
+    }
+
+    // Set the server to listen on the socket
+    if (listen(sockfd, BACKLOG) == -1) 
+    {
+        // If the server cannot listen on the socket, throw an error
+        perror("[SERVER]: listen\n");
         exit(1);
     }
 
-    if (listen(sockfd, BACKLOG) == -1) {
-        perror("listen");
+    return sockfd;
+}
+
+void echo_mode(int new_fd) 
+{
+    char buf[MAXDATASIZE];
+    int numbytes;
+
+    printf("[SERVER]: Entering Echo Mode...\n");
+
+    // While the server is receiving messages
+    while ((numbytes = recv(new_fd, buf, MAXDATASIZE - 1, 0)) > 0) 
+    {
+        buf[numbytes] = '\0';
+
+        printf("[SERVER]: Received message from server: '%s'\n", buf);
+        if (strcmp(buf, "close") == 0) 
+        {
+            // If the received message is 'close', send 'goodbye'
+            send(new_fd, "Goodbye", 7, 0);
+            printf("[SERVER]: Sent message to client: '%s'\n", "Goodbye");
+            break;
+        }
+
+        // Send the message that was received back to the client
+        send(new_fd, buf, strlen(buf), 0);
+        printf("[SERVER]: Sent message to client: '%s'\n", buf);
+    }
+
+    if (numbytes == -1) 
+    {
+        // If a message wasn't received, throw an error
+        perror("[SERVER]: recv\n");
+    }
+}
+
+void file_transfer(int new_fd) 
+{
+    FILE *file = fopen("file.txt", "r"); // Open file for reading
+    char buf[MAXDATASIZE];
+    size_t bytesRead;
+
+    printf("[SERVER]: Entering File Transfer Mode...\n");
+
+    if (file == NULL) 
+    {
+        // If there's no file, throw an error
+        fprintf(stderr, "[SERVER]: ERROR: Cannot open file!\n");
+        return;
+    }
+
+    while ((bytesRead = fread(buf, 1, MAXDATASIZE, file)) > 0) 
+    {
+        // Send the file to the client
+        if (send(new_fd, buf, bytesRead, 0) == -1) 
+        {
+            // If the file fails to send, throw an error
+            perror("[SERVER]: send\n");
+            fclose(file);
+            return;
+        }
+    }
+
+    fclose(file);
+    printf("[SERVER]: File transfer completed successfully!\n");
+}
+
+void handle_client(int new_fd) 
+{
+    char buf[MAXDATASIZE];
+    int numbytes;
+
+    // Receive the selected mode from the client
+    if ((numbytes = recv(new_fd, buf, MAXDATASIZE - 1, 0)) <= 0) 
+    {
+        // If no valid mode was received, throw an error
+        if (numbytes == -1) perror("[SERVER]: recv\n");
+        close(new_fd);
+        return;
+    }
+
+    buf[numbytes] = '\0';
+
+    // Execute one of the modes
+    if (strcmp(buf, "1") == 0) {
+        // If the mode is 1, process echo mode
+        echo_mode(new_fd);
+    } else if (strcmp(buf, "2") == 0) {
+        // If the mode is 2, process file transfer mode
+        file_transfer(new_fd);
+    } else {
+        // If the mode isn't 1 or 2, throw an error
+        printf("[SERVER]: Unknown mode!\n");
+    }
+
+    close(new_fd);
+}
+
+int main(void) 
+{
+    int sockfd = setup_server(); // Set up the server
+    if (sockfd == -1)
+    {
+        // If the set up fails, terminate program
         exit(1);
     }
 
-    sa.sa_handler = sigchld_handler; // reap all dead processes
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;
-    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-        perror("sigaction");
-        exit(1);
-    }
+    struct sockaddr_storage their_addr;
+    socklen_t sin_size;
+    char ipstr[INET6_ADDRSTRLEN];
+    int new_fd;
 
-    printf("[SERVER]: waiting for connections... \n");
+    printf("[SERVER]: waiting for connections...\n");
 
-    while(1) 
-    {  // main accept() loop
+    while (1) {
         sin_size = sizeof their_addr;
+
+        // Accept the connection from the client
         new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-        if (new_fd == -1) {
-            perror("accept");
+        if (new_fd == -1) 
+        {
+            perror("[SERVER]: accept\n");
             continue;
         }
 
-        inet_ntop(their_addr.ss_family,
-            get_in_addr((struct sockaddr *)&their_addr),
-            s, sizeof s);
-        printf("[SERVER]: got connection from %s\n", s);
+        // Get the client's IP address
+        inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), ipstr, sizeof ipstr);
+        printf("[SERVER]: got connection from %s\n", ipstr);
 
-        if (!fork()) { // this is the child process
-            close(sockfd); // child doesn't need the listener
-
-            // Receive the mode from the client
-            if ((numbytes = recv(new_fd, buf, MAXDATASIZE-1, 0)) == -1) {
-                perror("recv");
-                close(new_fd);
-                exit(1);
-            }
-            buf[numbytes] = '\0'; // Null-terminate the received string
-
-            // Parse the mode
-            if (strcmp(buf, "1") == 0) { // Echo mode
-                char *mode_init = "[SERVER]: Entered Echo Mode!";
-                char echo[MAXDATASIZE];
-
-                printf("%s\n", mode_init);
-
-                // Send acknowledgment to the client
-                if (send(new_fd, mode_init, strlen(mode_init), 0) == -1) {
-                    perror("[SERVER]: send");
-                    close(new_fd);
-                    exit(1);
-                }
-
-                while (1) { // Echo loop
-                    if ((numbytes = recv(new_fd, echo, MAXDATASIZE-1, 0)) == -1) {
-                        perror("[SERVER]: recv");
-                        break;
-                    }
-
-                    echo[numbytes] = '\0'; // Null-terminate received message
-
-                    if (strcmp(echo, "close") == 0) { // Client wants to end the connection
-                        // Send a goodbye message before closing
-                        if (send(new_fd, "Goodbye", 7, 0) == -1) {
-                            perror("[SERVER] send");
-                        }
-
-                        printf("[SERVER]: Closed client connection");
-                        break;
-                    }
-
-                    // Echo the message back to the client
-                    if (send(new_fd, echo, strlen(echo), 0) == -1) {
-                        perror("send");
-                        break;
-                    }
-
-                    printf("[SERVER]: echoed: '%s'\n", echo);
-                }
-            } else if (strcmp(buf, "2") == 0) { // File Transfer mode
-                char *mode_init = "[SERVER]: Entered File Transfer Mode!";
-                FILE *file = fopen("file.txt", "r");
-                char line[MAXDATASIZE];
-                size_t bytesRead;
-
-                printf("%s\n", mode_init);
-
-                // Send acknowledgment to the client
-                if (send(new_fd, mode_init, strlen(mode_init), 0) == -1) {
-                    perror("[SERVER]: send");
-                    close(new_fd);
-                    exit(1);
-                }
-
-                if (file == NULL) {
-                    fprintf(stderr, "ERROR: Cannot open file!\n");
-                    printf("[SERVER]: Failed to transfer file.\n");
-                } else {
-                    while ((bytesRead = fread(line, 1, MAXDATASIZE, file)) > 0) {
-                        // Send the chunk to the client
-                        if (send(new_fd, line, bytesRead, 0) == -1) {
-                            perror("[SERVER]: send");
-                            fclose(file);
-                            close(new_fd);
-                            exit(1);
-                        }
-                        printf("[SERVER]: Sent %zu bytes from file\n", bytesRead);
-                    }
-
-                    if (ferror(file)) {
-                        fprintf(stderr, "ERROR: File read error occurred!\n");
-                    } else {
-                        printf("[SERVER]: File transfer completed successfully.\n");
-                    }
-
-                    fclose(file);
-                }
-
-                close(new_fd); // Close the connection
-            } else {
-                printf("ERROR: Unknown mode\n");
-            }
-
-            close(new_fd); // Close the connection
+        if (!fork()) 
+        {
+            close(sockfd);
+            handle_client(new_fd);
             exit(0);
         }
 
-        close(new_fd);  // parent doesn't need this
+        close(new_fd);
     }
+
     return 0;
 }
